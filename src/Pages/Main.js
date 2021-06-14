3a2e3ae9c9c31eb6403e62667832f3fa833c8259
Cody Durham <codydd@gmail.com>
2021-06-14 10:59:19 -0600

    Initial page & component setup / not complete

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,7 +1,52 @@
 import react from 'react'
 
+import ContentContainer from '../Components/ContentContainer'
+
+
+// import ChatButton from './Componets/ChatButton';
+// import FilterAndSortModal from './Componets/FilterAndSortModal';
+// import ProductCard from './Componets/ProductCard';
+// import ProductCardFavorite from './Componets/ProductCardFavorite';
+// import ReturnToTopButton from './Componets/ReturnToTopButton';
+// import ShowingSticky from './Componets/ShowingSticky';
+// import SideBarNav from './Componets/SideBarNav';
+// import SignInModal from './Componets/SignInModal';
+// import StikyFilterHeader from './Componets/StickyFilterHeader.jsHamburgerDropdown';
+
+
+
+
+
 const Main = () => {
-return <h1>this is the Main PAGE</h1>
+    return (
+        <>
+        <h1>this is the Main PAGE</h1>
+        <ContentContainer>
+            <p>Audience</p>
+        </ContentContainer>
+
+        {/* <ContentContainer> */}
+            {/* {/:audience/:category} */}
+        {/* </ContentContainer> */}
+
+        {/* <ContentContainer> */}
+            {/* {/:audience/:category/:group} */}
+        {/* </ContentContainer> */}
+
+        </>
+    )
 }
 
 export default Main
+
+
+
+    //  {/* <ChatButton/> */}
+    //  {/* <FilterAndSortModal/> */}
+    //  {/* <ProductCard/> */}
+    //  {/* <ProductCardFavorite/> */}
+    //  {/* <ReturnToTopButton/> */}
+    //  {/* <ShowingSticky/> */}
+    //  {/* <SideBarNav/> */}
+    //  {/* <SignInModal/> */}
+    //  {/* <StikyFilterHeader/> */}
