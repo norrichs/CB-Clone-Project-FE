788fca60cf5d34a12b9e1c8aad17d4dfb3057363
ben.norrichs@gmail.com <ben.norrichs@gmail.com>
2021-06-18 16:26:34 -0400

    added getProductFamilesForCategory and control logic

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -15,11 +15,12 @@ const Main = ({ awsURL, imgBaseURL }, props) => {
 	// console.log('awsurl', awsURL)
 	// console.log("these are the params", useParams());
 	const { audience, category, group } = useParams();
+	console.log('params', audience, category, group)
 	const location = useLocation()
 	const [contentDisplay, setContentDisplay] = React.useState([])
 	
 	// console.log(awsURL + "/product/" + audience + "/" + category + "/" + group)
-	const getProductFamilies = async () => {
+	const getProductFamiliesForGroup = async () => {
 		// console.log('fetching: ' + awsURL + "/product/" + audience + "/" + category + "/" + group + "/")
 		fetch(awsURL + "/product/" + audience + "/" + category + "/" + group)
 			.then((res) => {
@@ -48,10 +49,38 @@ const Main = ({ awsURL, imgBaseURL }, props) => {
 				}))
 			})
 	}
-
+	const getProductFamiliesForCategory = async () => {
+		console.log('fetching: ' + awsURL + "/product/" + audience + "/" + category + "/")
+		fetch(awsURL + "/product/" + audience + "/" + category + "/")
+			.then((res) => {
+				// console.log('get families response', res)
+				return res.json()
+			})
+			.then((data)=>{
+				// console.log('got data', data)
+				setContentDisplay(data.body.Items.map((pFam, i)=> {
+					console.log('setContentDisplay product family data: ', pFam)
+					return (<ProductCard 
+						key={i}
+						imgBaseURL={imgBaseURL} 
+						pFam={{
+							audience: pFam.audience,
+							category: pFam.category,
+							group: pFam.group,
+							price: pFam.price,
+							price_sale: pFam.price_sale,
+							image0: pFam.images[0],
+							image1: pFam.images[1],
+							alt0: pFam.altText[0],
+							alt1: pFam.altText[1],
+							label: pFam.product_family
+						}}/>)
+				}))
+			})
+	}
 
 	useEffect(()=>{
-		getProductFamilies()
+		group === undefined ? getProductFamiliesForCategory() : getProductFamiliesForGroup() 
 	},[location])
 	return (
 		<main >
